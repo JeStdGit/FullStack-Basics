a6a16db1b54a69ab77ddf4a82adc32aa27a35fe4
jacobel640 <jacobel640@gmail.com>
2026-06-14 20:19:39 +0300

    added vannila-js classes

---
+++ 03-vannila-js/01-intro/script.js
@@ -0,0 +1,12 @@
+console.log("Hello World");
+
+function greetPerson(message, name) {
+    console.log(`${message} ${name}`);
+}
+
+let msg = "I want to pless you Mr."
+let name = "John Doe"
+
+greetPerson(msg, name)
+
+document.title = "New Title"
